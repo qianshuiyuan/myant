eceb9860282a793d370cd54dadb8b5accc67a334
lxz <243739113@qq.com>
2017-12-06 16:44:35 +0800

    updata

---
+++ Include/My/File.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ronda
+ * Date: 2017/12/6
+ * Time: 16:35
+ */
+namespace My;
+class File
+{
+    public function info()
+    {
+        echo "这个文件管理类型，包括图片等的上传";
+    }
+
+}
